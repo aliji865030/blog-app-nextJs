eaaaf3eb3b6880831630307a9460c75925fcbaad
aliji865030 <abbasalichand786@gmail.com>
2024-10-14 08:00:08 +0530

    bloglist added

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -7,6 +7,11 @@ const Header = () => {
     <div className='px-5 py-5 md:px-12 lg:px-28'>
       <div className='flex justify-between items-center'>
         <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto'/>
+        <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]'> Get started <Image src={assets.arrow}/> </button>
+      </div>
+      <div className="text-center my-8">
+        <h1 className="text-3xl sm:text-5xl font-medium">Latest Blogs</h1>
+        <p className="mt-10 max-w-[740px] m-auto text-xs sm:text-base"> Welcome to your digital diary! Unleash your thoughts, capture moments, and share your stories with the world, all from the comfort of your pocket. Start writing, and let your voice be heard.</p>
       </div>
     </div>
   )
--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
+"use client"
+import Blogitem from "@/Components/Blogitem";
+import BlogList from "@/Components/BlogList";
 import Header from "@/Components/Header";
 
 export default function Home() {
   return (
     <>
     <Header/>
+    {/* <Blogitem/> */}
+    <BlogList/>
     </>
   );
 }
